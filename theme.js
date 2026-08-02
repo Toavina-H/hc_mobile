a16bd470afb7e64b4231bc62360e683362842909
Toavina <dev.04@crea-tic.net>
2026-08-02 22:24:01 +0300

    install react native paper

---
+++ theme.js
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#6308F7',
+    secondary: '#00A7E5',
+    positive:'#00CC67',
+    error: '#FD0F64',
+  },
+}
